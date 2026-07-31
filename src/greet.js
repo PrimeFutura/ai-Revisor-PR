fb24a21409b8da44bd30ab4faa3796b238550b09
findecano <jacruzjacruz@gmail.com>
2026-07-30 20:53:01 -0500

    Fase 2: obtener diff y archivos cambiados del PR

--- src/greet.js
+++ src/greet.js
@@ -2,7 +2,7 @@
 // en las ramas feature y así generar un diff en los Pull Requests.
 
 export function greet(name = "equipo") {
-  return `Hola, ${name}!`;
+  return `Hey, ${name}!`;
 }
 
 // Ejecutable directo: `npm start`
