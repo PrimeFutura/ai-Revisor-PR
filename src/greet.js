653d8261584075c63f8425bc0ceb13b046478d25
findecano <jacruzjacruz@gmail.com>
2026-07-30 20:16:55 -0500

    feat: saludo por defecto a 'equipo'

--- src/greet.js
+++ src/greet.js
@@ -1,7 +1,7 @@
 // Código de ejemplo. Sirve para tener "algo real" que modificar
 // en las ramas feature y así generar un diff en los Pull Requests.
 
-export function greet(name = "mundo") {
+export function greet(name = "equipo") {
   return `Hola, ${name}!`;
 }
 
